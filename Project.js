281e8f92166c55a1dd5017f48a36264fab25ed54
Jenna rose <jemichel@youth.dhftc.org>
2016-07-01 15:16:01 -0400

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,25 @@
+var  = document.getElementById("");
+test.innerHTML = '';
+// Create a new 'div' element and store it in a variable
+var myNewDiv = document.createElement("div");
+// Append the new Div to your page
+document.body.appendChild(myNewDiv);
+// create a new variable to store this text
+var text = document.createTextNode("this is a sample text that was added through JavaScript")
+// Append the text to the new Div
+myNewDiv.appendChild(text);
+document.getElementById("WebTitle");
+var textchange = "this is what the header will say once the change is loaded!";
+}
+function mySecondFunction() {
+    console.log("wassup");
+};
+    
+        
+var myButton = document.getElementById('myButton');
+                   
+                     
+myButton.onclick = function (){
+    console.log('Soccer!');
+};
+var myButton = document.GetElementById{'myButton'}
